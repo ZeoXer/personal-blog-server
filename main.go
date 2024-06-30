c27c57b71862906a2a490077a3bdfb99d5ec13dd
ZeoXer <andrew17413@gmail.com>
2024-07-01 01:16:03 +0800

    add cors settings

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"go-server/global"
 	"go-server/router"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -24,6 +25,12 @@ func main() {
 
 	// Initialize the routes
 	router.InitializeRoutes(server)
+
+	corsConfig := cors.DefaultConfig()
+	corsConfig.AllowOrigins = []string{"https://blog.zeoxer.com"}
+	corsConfig.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	corsConfig.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Authorization"}
+	server.Use(cors.New(corsConfig))
 
 	// Set the trusted proxies to nil
 	server.SetTrustedProxies(nil)
